ef2ce25bd90dca35bb99a6e1ca08edfbdd3678b1
Justin Owusu <justinowusu@google.com>
2020-09-08 18:03:38 -0700

    Completed implementation of the Prio data share serialization tool.

--- src/main/java/com/google/exposurenotification/privateanalytics/ingestion/PrioSerializer.java
+++ src/main/java/com/google/exposurenotification/privateanalytics/ingestion/PrioSerializer.java
@@ -1,5 +1,12 @@
 package com.google.exposurenotification.privateanalytics.ingestion;
 
+import ENPA.PrioBatchHeader;
+import ENPA.PrioBatchHeaderSignature;
+import ENPA.PrioDataSharePacket;
+import java.io.File;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
 import org.apache.avro.file.DataFileReader;
 import org.apache.avro.file.DataFileWriter;
 import org.apache.avro.io.DatumReader;
@@ -7,36 +14,94 @@ import org.apache.avro.io.DatumWriter;
 import org.apache.avro.specific.SpecificDatumReader;
 import org.apache.avro.specific.SpecificDatumWriter;
 
-import java.io.File;
-import java.io.IOException;
-import java.util.ArrayList;
-import java.util.List;
-
 /**
- * Tool for serializing/deserializing Prio data shares into the Apache Avro file format.
+ * Tool for serializing and deserializing Prio data shares into (or from) the Apache Avro file
+ * format.
  */
-
 public class PrioSerializer {
-    public static void serializePrioBatchHeaders(List<ENPA.PrioBatchHeader> prioBatchHeaders, String pathname) throws IOException {
-        DatumWriter<ENPA.PrioBatchHeader> pbhDatumWriter = new SpecificDatumWriter<ENPA.PrioBatchHeader>(ENPA.PrioBatchHeader.class);
-        DataFileWriter<ENPA.PrioBatchHeader> dataFileWriter = new DataFileWriter<ENPA.PrioBatchHeader>(pbhDatumWriter);
-        dataFileWriter.create(ENPA.PrioBatchHeader.getClassSchema(), new File(pathname));
-        for (ENPA.PrioBatchHeader prioBatchHeader: prioBatchHeaders) {
+    public static void serializeBatchHeaders(List<PrioBatchHeader> prioBatchHeaders, String pathname)
+            throws IOException {
+        DatumWriter<PrioBatchHeader> batchHeaderDatumWriter =
+                new SpecificDatumWriter<>(PrioBatchHeader.class);
+        DataFileWriter<PrioBatchHeader> dataFileWriter = new DataFileWriter<>(batchHeaderDatumWriter);
+        dataFileWriter.create(PrioBatchHeader.getClassSchema(), new File(pathname));
+        for (PrioBatchHeader prioBatchHeader : prioBatchHeaders) {
             dataFileWriter.append(prioBatchHeader);
         }
         dataFileWriter.close();
     }
 
-    public static List<ENPA.PrioBatchHeader> deserializePrioBatchHeaders(String pathname) throws IOException {
-        DatumReader<ENPA.PrioBatchHeader> pbhDatumReader = new SpecificDatumReader<ENPA.PrioBatchHeader>(ENPA.PrioBatchHeader.class);
-        DataFileReader<ENPA.PrioBatchHeader> dataFileReader = new DataFileReader<ENPA.PrioBatchHeader>(new File(pathname), pbhDatumReader);
-        List<ENPA.PrioBatchHeader> prioBatchHeaders = new ArrayList<>();
-        ENPA.PrioBatchHeader prioBatchHeader = null;
+    public static void serializeBatchHeaderSignatures(
+            List<ENPA.PrioBatchHeaderSignature> prioBatchHeaderSignatures, String pathname)
+            throws IOException {
+        DatumWriter<PrioBatchHeaderSignature> signatureDatumWriter =
+                new SpecificDatumWriter<>(ENPA.PrioBatchHeaderSignature.class);
+        DataFileWriter<PrioBatchHeaderSignature> dataFileWriter =
+                new DataFileWriter<>(signatureDatumWriter);
+        dataFileWriter.create(PrioBatchHeaderSignature.getClassSchema(), new File(pathname));
+        for (PrioBatchHeaderSignature prioBatchHeaderSignature : prioBatchHeaderSignatures) {
+            dataFileWriter.append(prioBatchHeaderSignature);
+        }
+        dataFileWriter.close();
+    }
+
+    public static void serializeDataSharePackets(
+            List<PrioDataSharePacket> prioDataSharePackets, String pathname) throws IOException {
+        DatumWriter<PrioDataSharePacket> dataShareDatumWriter =
+                new SpecificDatumWriter<>(PrioDataSharePacket.class);
+        DataFileWriter<PrioDataSharePacket> dataFileWriter = new DataFileWriter<>(dataShareDatumWriter);
+        dataFileWriter.create(PrioDataSharePacket.getClassSchema(), new File(pathname));
+        for (PrioDataSharePacket prioDataSharePacket : prioDataSharePackets) {
+            dataFileWriter.append(prioDataSharePacket);
+        }
+        dataFileWriter.close();
+    }
+
+    public static List<ENPA.PrioBatchHeader> deserializeBatchHeaders(String pathname)
+            throws IOException {
+        DatumReader<PrioBatchHeader> batchHeaderDatumReader =
+                new SpecificDatumReader<>(PrioBatchHeader.class);
+        DataFileReader<PrioBatchHeader> dataFileReader =
+                new DataFileReader<>(new File(pathname), batchHeaderDatumReader);
+        List<PrioBatchHeader> prioBatchHeaders = new ArrayList<>();
+        PrioBatchHeader prioBatchHeader;
         while (dataFileReader.hasNext()) {
-            prioBatchHeader =  new ENPA.PrioBatchHeader();
+            prioBatchHeader = new PrioBatchHeader();
             prioBatchHeader = dataFileReader.next(prioBatchHeader);
             prioBatchHeaders.add(prioBatchHeader);
         }
         return prioBatchHeaders;
+    }
+
+    public static List<PrioBatchHeaderSignature> deserializeBatchHeaderSignatures(String pathname)
+            throws IOException {
+        DatumReader<PrioBatchHeaderSignature> signatureDatumReader =
+                new SpecificDatumReader<>(PrioBatchHeaderSignature.class);
+        DataFileReader<PrioBatchHeaderSignature> dataFileReader =
+                new DataFileReader<>(new File(pathname), signatureDatumReader);
+        List<PrioBatchHeaderSignature> prioBatchHeaderSignatures = new ArrayList<>();
+        PrioBatchHeaderSignature prioBatchHeaderSignature;
+        while (dataFileReader.hasNext()) {
+            prioBatchHeaderSignature = new PrioBatchHeaderSignature();
+            prioBatchHeaderSignature = dataFileReader.next(prioBatchHeaderSignature);
+            prioBatchHeaderSignatures.add(prioBatchHeaderSignature);
+        }
+        return prioBatchHeaderSignatures;
+    }
+
+    public static List<PrioDataSharePacket> deserializeDataSharePackets(String pathname)
+            throws IOException {
+        DatumReader<PrioDataSharePacket> dataShareDatumReader =
+                new SpecificDatumReader<>(PrioDataSharePacket.class);
+        DataFileReader<PrioDataSharePacket> dataFileReader =
+                new DataFileReader<>(new File(pathname), dataShareDatumReader);
+        List<PrioDataSharePacket> prioDataSharePackets = new ArrayList<>();
+        PrioDataSharePacket prioDataSharePacket;
+        while (dataFileReader.hasNext()) {
+            prioDataSharePacket = new PrioDataSharePacket();
+            prioDataSharePacket = dataFileReader.next(prioDataSharePacket);
+            prioDataSharePackets.add(prioDataSharePacket);
+        }
+        return prioDataSharePackets;
     }
 }
--- src/test/java/com/google/exposurenotification/privateanalytics/ingestion/PrioSerializerTest.java
+++ src/test/java/com/google/exposurenotification/privateanalytics/ingestion/PrioSerializerTest.java
@@ -1,64 +1,117 @@
 package com.google.exposurenotification.privateanalytics.ingestion;
 
-import org.junit.Rule;
-import org.junit.Test;
-import org.junit.rules.TemporaryFolder;
-import org.junit.runner.RunWith;
-import org.junit.runners.JUnit4;
+import static org.junit.Assert.assertEquals;
 
 import java.io.File;
 import java.io.IOException;
 import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.List;
+import org.junit.Rule;
+import org.junit.Test;
+import org.junit.rules.TemporaryFolder;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 
-import static org.junit.Assert.assertEquals;
-
-/**
- * Tests for {@link PrioSerializer}.
- */
-
+/** Tests for {@link PrioSerializer}. */
 @RunWith(JUnit4.class)
-public class PrioSerializerTest{
+public class PrioSerializerTest {
 
-    @Rule
-    public TemporaryFolder tmpFolder = new TemporaryFolder();
+    @Rule public TemporaryFolder tmpFolder = new TemporaryFolder();
 
     @Test
     public void testPrioBatchHeaderSerialization() throws IOException {
-        List<ENPA.PrioBatchHeader> prioBatchHeaders = new ArrayList<>();
-        ENPA.PrioBatchHeader pbh1 = ENPA.PrioBatchHeader.newBuilder()
-                .setBatchUuid("id123")
-                .setName("secretname")
-                .setBins(123)
-                .setEpsilon(3.14)
-                .setPrime(7)
-                .setNumberOfServers(3)
-                .setHammingWeight(5)
-                .setBatchStartTime(1600000000)
-                .setBatchEndTime(1700000000)
-                .setCertificateHash("hashyhash")
-                .setSignatureOfPackets(ByteBuffer.wrap(new byte[] {0x10, 0x20, 0x30}))
-                .build();
-        ENPA.PrioBatchHeader pbh2 = ENPA.PrioBatchHeader.newBuilder()
-                .setBatchUuid("id987")
-                .setName("simplename")
-                .setBins(4)
-                .setEpsilon(2.71)
-                .setPrime(13)
-                .setNumberOfServers(5)
-                .setHammingWeight(8)
-                .setBatchStartTime(1650000000)
-                .setBatchEndTime(1710000000)
-                .setCertificateHash("hashedpotatoes")
-                .setSignatureOfPackets(ByteBuffer.wrap(new byte[] {0x12, 0x13, 0x14}))
-                .build();
+        List<ENPA.PrioBatchHeader> batchHeaders = new ArrayList<>();
+        ENPA.PrioBatchHeader header1 =
+                ENPA.PrioBatchHeader.newBuilder()
+                        .setBatchUuid("id123")
+                        .setName("secretname")
+                        .setBins(123)
+                        .setEpsilon(3.14)
+                        .setPrime(7)
+                        .setNumberOfServers(3)
+                        .setHammingWeight(5)
+                        .setBatchStartTime(1600000000)
+                        .setBatchEndTime(1700000000)
+                        .setCertificateHash("hashyhash")
+                        .setSignatureOfPackets(ByteBuffer.wrap(new byte[] {0x10, 0x20, 0x30}))
+                        .build();
+        ENPA.PrioBatchHeader header2 =
+                ENPA.PrioBatchHeader.newBuilder()
+                        .setBatchUuid("id987")
+                        .setName("simplename")
+                        .setBins(4)
+                        .setEpsilon(2.71)
+                        .setPrime(13)
+                        .setNumberOfServers(5)
+                        .setHammingWeight(8)
+                        .setBatchStartTime(1650000000)
+                        .setBatchEndTime(1710000000)
+                        .setCertificateHash("hashedpotatoes")
+                        .setSignatureOfPackets(ByteBuffer.wrap(new byte[] {0x12, 0x13, 0x14}))
+                        .build();
+        batchHeaders.add(header1);
+        batchHeaders.add(header2);
 
-        prioBatchHeaders.add(pbh1);
-        prioBatchHeaders.add(pbh2);
         File serializedHeaders = tmpFolder.newFile();
-        PrioSerializer.serializePrioBatchHeaders(prioBatchHeaders, serializedHeaders.getAbsolutePath());
-        List<ENPA.PrioBatchHeader> deserializedHeaders = PrioSerializer.deserializePrioBatchHeaders(serializedHeaders.getAbsolutePath());
-        assertEquals(prioBatchHeaders, deserializedHeaders);
+        PrioSerializer.serializeBatchHeaders(batchHeaders, serializedHeaders.getAbsolutePath());
+        List<ENPA.PrioBatchHeader> deserializedHeaders =
+                PrioSerializer.deserializeBatchHeaders(serializedHeaders.getAbsolutePath());
+        assertEquals(batchHeaders, deserializedHeaders);
+    }
+
+    @Test
+    public void testPrioBatchHeaderSignatureSerialization() throws IOException {
+        List<ENPA.PrioBatchHeaderSignature> signatures = new ArrayList<>();
+        ENPA.PrioBatchHeaderSignature signature1 =
+                ENPA.PrioBatchHeaderSignature.newBuilder()
+                        .setBatchHeaderSignature(ByteBuffer.wrap(new byte[] {0x01, 0x02, 0x03}))
+                        .build();
+        ENPA.PrioBatchHeaderSignature signature2 =
+                ENPA.PrioBatchHeaderSignature.newBuilder()
+                        .setBatchHeaderSignature(ByteBuffer.wrap(new byte[] {0x04, 0x05, 0x06}))
+                        .build();
+        signatures.add(signature1);
+        signatures.add(signature2);
+
+        File serializedSignatures = tmpFolder.newFile();
+        PrioSerializer.serializeBatchHeaderSignatures(
+                signatures, serializedSignatures.getAbsolutePath());
+        List<ENPA.PrioBatchHeaderSignature> deserializedSignatures =
+                PrioSerializer.deserializeBatchHeaderSignatures(serializedSignatures.getAbsolutePath());
+        assertEquals(signatures, deserializedSignatures);
+    }
+
+    @Test
+    public void testPrioDataSharePacketSerialization() throws IOException {
+        List<ENPA.PrioDataSharePacket> dataSharePackets = new ArrayList<>();
+        ENPA.PrioDataSharePacket dataSharePacket1 =
+                ENPA.PrioDataSharePacket.newBuilder()
+                        .setDeviceNonce(ByteBuffer.wrap(new byte[] {0x07, 0x08, 0x09}))
+                        .setEncryptionKeyId("verysecretandsecurevalue1")
+                        .setRPit(1234567890)
+                        .setUuid("uniqueuserid1")
+                        .setVersionConfiguration("v1.0")
+                        .setEncryptedPayload(ByteBuffer.wrap(new byte[] {0x01, 0x02, 0x03, 0x04, 0x05}))
+                        .build();
+
+        ENPA.PrioDataSharePacket dataSharePacket2 =
+                ENPA.PrioDataSharePacket.newBuilder()
+                        .setDeviceNonce(ByteBuffer.wrap(new byte[] {0x10, 0x11, 0x12}))
+                        .setEncryptionKeyId("verysecretandsecurevalue2")
+                        .setRPit(987654321)
+                        .setUuid("uniqueuserid2")
+                        .setVersionConfiguration("v2.0")
+                        .setEncryptedPayload(ByteBuffer.wrap(new byte[] {0x06, 0x07, 0x08, 0x09, 0x10}))
+                        .build();
+        dataSharePackets.add(dataSharePacket1);
+        dataSharePackets.add(dataSharePacket2);
+
+        File serializedDataShares = tmpFolder.newFile();
+        PrioSerializer.serializeDataSharePackets(
+                dataSharePackets, serializedDataShares.getAbsolutePath());
+        List<ENPA.PrioDataSharePacket> deserializedHeaders =
+                PrioSerializer.deserializeDataSharePackets(serializedDataShares.getAbsolutePath());
+        assertEquals(dataSharePackets, deserializedHeaders);
     }
 }
