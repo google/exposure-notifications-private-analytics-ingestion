4160962da53f0dfc47343b8d40c178345f89ca2c
Guray Alsac <guray@google.com>
2020-09-14 17:10:28 -0700

    fix security rule read test

--- config/firebase/rules.test.js
+++ config/firebase/rules.test.js
@@ -9,28 +9,31 @@ beforeAll(async () => {
     projectId: projectId,
     rules: fs.readFileSync("firestore.rules", "utf8")
   });
-});
-
-beforeEach(async () => {
-  await firebase.clearFirestoreData({projectId});
+  const app = firebase.initializeAdminApp({ projectId: projectId });
+  const doc = app.firestore().collection('uuid').doc('preexisting')
+                .collection('date').doc('2020-09-03-13')
+                .collection('metrics').doc('testMetric');
+  await doc.set({
+    'created': firebase.firestore.FieldValue.serverTimestamp(),
+    'uuid': 'preexisting'});
 });
 
 afterAll(async () => {
   await Promise.all(firebase.apps().map((app) => app.delete()));
 });
 
-describe('Tests of document creation', () => {
+describe('Tests of document writes and access', () => {
   const app = firebase.initializeTestApp({
     projectId: projectId,
     auth: null
   });
   const db = app.firestore()
-  it('document cannot be created without uuid',
+  it('document cannot be written without uuid',
       async () => {
         const doc = db.collection('uuid').doc('foo');
         await firebase.assertFails(doc.set({}));
       });
-  it('document cannot be created without created',
+  it('document cannot be written without created',
       async () => {
         const doc = db.collection('uuid').doc('foo');
         await firebase.assertFails(doc.set({
@@ -59,12 +62,11 @@ describe('Tests of document creation', () => {
                       .collection('metrics').doc('testMetric');
         await firebase.assertFails(doc.update({'uuid': 'foo'}));
       });
-// TODO: workaround jest issue and uncomment: https://github.com/firebase/firebase-js-sdk/issues/3096
-//  it('document cannot be read from server',
-//      async () => {
-//        const doc = db.collection('uuid').doc('foo')
-//                      .collection('date').doc('2020-09-03-14')
-//                      .collection('metrics').doc('testMetric');
-//        await firebase.assertFails(doc.get({ source: 'server' }));
-//      });
+  it('document cannot be read',
+      async () => {
+        const doc = db.collection('uuid').doc('preexisting')
+                      .collection('date').doc('2020-09-03-13')
+                      .collection('metrics').doc('testMetric');
+        await firebase.assertFails(doc.get());
+      });
 });
