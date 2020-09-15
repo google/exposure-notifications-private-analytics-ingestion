68c75e1ccb7832f77f0402560460454d86584881
Larry Jacobs <larryjacobs@google.com>
2020-09-15 16:41:23 -0700

    Fixed integration tests.

--- src/test/java/com/google/exposurenotification/privateanalytics/ingestion/IngestionPipelineIT.java
+++ src/test/java/com/google/exposurenotification/privateanalytics/ingestion/IngestionPipelineIT.java
@@ -1,10 +1,11 @@
 package com.google.exposurenotification.privateanalytics.ingestion;
 
 import com.google.api.core.ApiFuture;
-import com.google.api.core.ApiFutures;
 import com.google.auth.oauth2.GoogleCredentials;
-import com.google.cloud.firestore.CollectionReference;
+import com.google.cloud.Timestamp;
+import com.google.cloud.firestore.DocumentReference;
 import com.google.cloud.firestore.Firestore;
+import com.google.cloud.firestore.WriteBatch;
 import com.google.cloud.firestore.WriteResult;
 import com.google.firebase.FirebaseApp;
 import com.google.firebase.FirebaseOptions;
@@ -12,7 +13,6 @@ import com.google.firebase.cloud.FirestoreClient;
 import java.io.File;
 import java.io.FileReader;
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -33,12 +33,13 @@ import org.junit.runners.JUnit4;
 @RunWith(JUnit4.class)
 public class IngestionPipelineIT {
 
-  static final long CREATION_TIME = 123L;
+  static final long CREATION_TIME = 12345L;
+  static final Timestamp CREATED = Timestamp.ofTimeSecondsAndNanos(CREATION_TIME, 0);
   static final String DOC_1_ID = "doc1";
   static final String DOC_2_ID = "doc2";
   static final String FIREBASE_PROJECT_ID = "emulator-test-project";
   static final long MINIMUM_PARTICIPANT_COUNT = 0L;
-  static final long DURATION = 1000000L;
+  static final long DURATION = 100000000L;
   static final String SERVICE_ACCOUNT_KEY_PATH = "PATH/TO/SERVICE_ACCOUNT_KEY.json";
   static final String TEST_COLLECTION_NAME = "test-uuid";
 
@@ -97,30 +98,24 @@ public class IngestionPipelineIT {
   /**
    * Creates test-users collection and adds sample documents to test queries.
    */
-  private static CollectionReference seedDatabase(Firestore db) throws Exception {
+  private static void seedDatabase(Firestore db) throws Exception {
     // Adding a wait here to give the Firestore instance time to initialize before attempting
     // to connect.
     TimeUnit.SECONDS.sleep(20);
-    CollectionReference uuids = db.collection(TEST_COLLECTION_NAME);
-    List<ApiFuture<WriteResult>> futures = new ArrayList<>();
-    Map<String, Object> doc1Data = new HashMap<>();
-    Map<String, Object> doc2Data = new HashMap<>();
-    doc1Data.put("id", DOC_1_ID);
-    doc1Data.put("created", CREATION_TIME);
-    doc2Data.put("id", DOC_2_ID);
-    doc2Data.put("created", CREATION_TIME);
-    futures.add(
-        uuids
-            .document("metric1")
-            .set(doc1Data));
-    futures.add(
-        uuids
-            .document("metric2")
-            .set(doc2Data));
 
-    // block on documents successfully added so test isn't flaky.
-    ApiFutures.allAsList(futures).get();
+    WriteBatch batch = db.batch();
 
-    return uuids;
+    DocumentReference doc1 = db.collection(TEST_COLLECTION_NAME).document(DOC_1_ID);
+    Map<String, Object> docData = new HashMap<>();
+    docData.put("created", CREATED);
+    batch.set(doc1, docData);
+
+    DocumentReference doc2 = db.collection(TEST_COLLECTION_NAME).document(DOC_2_ID);
+    batch.set(doc2, docData);
+    docData.put("created", CREATED);
+
+    ApiFuture<List<WriteResult>> future = batch.commit();
+    // future.get() blocks on batch commit operation
+    future.get();
   }
 }
